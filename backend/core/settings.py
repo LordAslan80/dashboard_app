9b7dc33ef2038e23dae22a17ed5c1aaec91b6dfe
Lord.Aslan <aslani80.m@gmail.com>
2026-02-20 16:28:12 +0330

    add reporting app to settings

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -40,6 +40,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Third-Party apps
+    "rest_framework",
+    # Local apps
+    "reporting.apps.ReportingConfig",
 ]
 
 MIDDLEWARE = [
