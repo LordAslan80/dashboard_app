835ae8942152287065444399a4d06a66fdf5e0f0
Lord.Aslan <aslani80.m@gmail.com>
2026-02-27 15:17:42 +0330

    add customer serializer

--- backend/reporting/serializers.py
+++ backend/reporting/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework.serializers import ModelSerializer
-from .models import Order
+from .models import Order, Customer
 
 
 class OrderSerializer(ModelSerializer):
     class Meta:
         model = Order
+        fields = "__all__"
+
+
+class CustomerSerializer(ModelSerializer):
+    class Meta:
+        model = Customer
         fields = "__all__"
