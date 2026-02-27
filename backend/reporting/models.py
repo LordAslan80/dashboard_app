fdca8e7d8b8f3b8b00dfb73805daf18a8ebbe7ad
Lord.Aslan <aslani80.m@gmail.com>
2026-02-27 14:42:10 +0330

    update order model

--- backend/reporting/models.py
+++ backend/reporting/models.py
@@ -2,6 +2,11 @@ from django.db import models
 
 
 class Order(models.Model):
-    amount = models.CharField(max_length=50)
-    description = models.CharField(max_length=100)
-    created_time = models.DateTimeField()
+    order_date = models.DateTimeField()
+    required_date = models.DateTimeField()
+    shipped_name = models.CharField(max_length=100)
+    shipped_city = models.CharField(max_length=100)
+    shipped_address = models.CharField(max_length=100)
+    shipped_postal_code = models.CharField(max_length=100)
+    shipped_country = models.CharField(max_length=100)
+    customer = models.ForeignKey(Customer, on_delete=models.DO_NOTHING)
