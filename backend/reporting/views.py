68b25353975a9e174282faea65d964bdcb4ddd03
Lord.Aslan <aslani80.m@gmail.com>
2026-02-28 10:58:22 +0330

    add category viewset

--- backend/reporting/views.py
+++ backend/reporting/views.py
@@ -1,6 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
-from .serializers import OrderSerializer
-from .models import Order
+from .serializers import OrderSerializer, CategorySerializer
+from .models import Order, Category
 
 
 class OrderViewSet(ModelViewSet):
@@ -8,3 +8,8 @@ class OrderViewSet(ModelViewSet):
 
     def get_queryset(self):
         return Order.objects.all().order_by("-order_date")
+
+
+class CategoryViewSet(ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
