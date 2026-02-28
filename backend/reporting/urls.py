b5b7dc5321c9c79f847dd420d4806b0b1285bc49
Lord.Aslan <aslani80.m@gmail.com>
2026-02-28 11:26:31 +0330

    update reporting app urls

--- backend/reporting/urls.py
+++ backend/reporting/urls.py
@@ -1,9 +1,20 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import OrderViewSet
+from .views import (
+    OrderViewSet,
+    CategoryViewSet,
+    ProductViewSet,
+    CustomerViewSet,
+    SupplierViewSet,
+)
 
 
 router = DefaultRouter()
 router.register("orders", OrderViewSet, basename="orders")
+router.register("categories", CategoryViewSet, basename="categories")
+router.register("products", ProductViewSet, basename="products")
+router.register("customers", CustomerViewSet, basename="customers")
+router.register("suppliers", SupplierViewSet, basename="suppliers")
+
 
 urlpatterns = [path("", include(router.urls))]
