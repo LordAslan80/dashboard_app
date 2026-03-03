524839e96356b51386dda8a531ad908d6be01aee
Lord.Aslan <aslani80.m@gmail.com>
2026-03-03 16:17:51 +0330

    configure pages structure

---
+++ frontend/src/views/relations/screens/CustomersView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        Customers View
+    </div>
+</template>
+
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent ({
+    
+})
+</script>
+
+
+<style lang="">
+    
+</style>
---
+++ frontend/src/views/reporting/screens/SuppliersView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        Suppliers View
+    </div>
+</template>
+
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent ({
+    
+})
+</script>
+
+
+<style lang="">
+    
+</style>
