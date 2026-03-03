24f0c22120acd4763e43b62e7bc4f373d600888e
Lord.Aslan <aslani80.m@gmail.com>
2026-03-03 16:15:27 +0330

    import scss styles for whole project

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -3,6 +3,15 @@ const webpack = require('webpack')
 
 module.exports = defineConfig({
   transpileDependencies: true,
+
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `@import "~@/styles/main.scss";`,
+      }
+    }
+  },
+
   configureWebpack: {
     plugins: [
       new webpack.DefinePlugin({
