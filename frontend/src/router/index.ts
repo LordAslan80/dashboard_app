839982367421c712792b7e8ecad27c22ed85b9dc
Lord.Aslan <aslani80.m@gmail.com>
2026-03-03 16:13:46 +0330

    add new router paths

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,11 +1,39 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import OrdersView from '@/views/reporting/screens/OrdersView.vue'
+import ProductsView from '@/views/reporting/screens/ProductsView.vue'
+import SuppliersView from '@/views/reporting/screens/SuppliersView.vue'
+import DashboardView from '@/views/DashboardView.vue'
+import CustomersView from '@/views/relations/screens/CustomersView.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'dashboard',
+    component: DashboardView,
+
+    children: [
+      {
+        path: '/orders',
+        name: 'orders',
+        component: OrdersView
+      },
+      {
+        path: '/products',
+        name: 'products',
+        component: ProductsView
+      },
+      {
+        path: '/suppliers',
+        name: 'suppliers',
+        component: SuppliersView
+      },
+      {
+        path: '/customers',
+        name: 'customers',
+        component: CustomersView,
+        meta: { screen: 'customers' }
+      }
+    ]
   },
 ]
 
