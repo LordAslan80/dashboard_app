4d459de4c4a265e5be0895ebdf18b29c51b19237
Lord.Aslan <aslani80.m@gmail.com>
2026-03-03 16:18:52 +0330

    create dashboard sidebar

---
+++ frontend/src/views/DashboardView.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="default-layout">
+        <aside>
+            <header class="logo">
+                <img :style="{height: '40%', width:'30%'}" src="@/assets/logo.png">
+                <h2>Reports</h2>
+            </header>
+            <nav>
+                <section class="nav-section">
+                    <div class="section-title">DASHBOARDS</div>
+                    <section>
+                        <a href="#">
+                            <span>Reporting</span>
+                        </a>
+                        <div class="subs">
+                            <router-link :to="{name: 'orders'}">Orders</router-link>
+                            <router-link :to="{name: 'products'}">Products</router-link>
+                            <router-link :to="{name: 'suppliers'}">Suppliers</router-link>
+                        </div>
+                    </section>
+                    <section>
+                        <a href="#">
+                            <span>Relations</span>
+                        </a>
+                        <div class="subs">
+                            <router-link :to="{name: 'customers'}">Customers</router-link>
+                        </div>
+                    </section>
+                </section>
+            </nav>
+        </aside>
+
+        <main class="body">
+            <router-view :key="route.path"></router-view>
+        </main>
+    </div>
+</template>
+
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { useRoute } from 'vue-router';
+
+export default defineComponent ({
+    setup() {
+        const route = useRoute()
+
+        return {
+            route
+        }
+    },
+})
+</script>
+
+
+<style lang="">
+    
+</style>
