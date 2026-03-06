91215af92581ce8c20685c14a7ad60ea4dfc1683
Lord.Aslan <aslani80.m@gmail.com>
2026-03-06 16:58:06 +0330

    connect orders page to backend and create its table

--- frontend/src/views/reporting/screens/OrdersView.vue
+++ frontend/src/views/reporting/screens/OrdersView.vue
@@ -1,15 +1,67 @@
 <template>
-    <div>
-        Orders View
+    <header>
+        <span class="title">Reporting / Orders</span>
+        <button class="button is-primary">New order</button>
+    </header>
+
+    <div class="body">
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Order date</th>
+                    <th>Customer name</th>
+                    <th>Product name</th>
+                    <th>Required date</th>
+                    <th>Shipped name</th>
+                    <th>Shipped address</th>
+                    <th>Shipped city</th>
+                    <th>Shipped postal code</th>
+                    <th>Shipped country</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(item, index) in orders" :key="index">
+                    <td>{{ item.id }}</td>
+                    <td>{{ item.order_date }}</td>
+                    <td>{{ item.customer.first_name }} {{ item.customer.last_name }}</td>
+                    <td>{{ item.product.product_name }}</td>
+                    <td>{{ item.required_date }}</td>
+                    <td>{{ item.shipped_name }}</td>
+                    <td>{{ item.shipped_address }}</td>
+                    <td>{{ item.shipped_city }}</td>
+                    <td>{{ item.shipped_postal_code }}</td>
+                    <td>{{ item.shipped_country }}</td>
+                    <td>
+                        <p>placeholder</p>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { loadOrders } from '@/api/reporting';
+import { defineComponent, onMounted, ref } from 'vue';
 
 export default defineComponent ({
-    
+    setup() {
+        const orders = ref()
+        const getOrders = async () => {
+            orders.value = await loadOrders()
+        }
+
+        onMounted(() => {
+            getOrders()
+        })
+
+        return {
+            orders
+        }
+    }
 })
 </script>
 
