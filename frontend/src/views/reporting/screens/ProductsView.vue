4ee2ff63b3c6296a438ff65a0b53c6df3efc2259
Lord.Aslan <aslani80.m@gmail.com>
2026-03-09 14:11:19 +0330

    configure products page

--- frontend/src/views/reporting/screens/ProductsView.vue
+++ frontend/src/views/reporting/screens/ProductsView.vue
@@ -1,15 +1,72 @@
 <template>
+    <header>
+        <span class="title">Reporting / Products</span>
+        <button class="button is-primary is-on-header">New Product</button>
+    </header>
     <div>
-        Products View
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Product name</th>
+                    <th>Category</th>
+                    <th>Unit price</th>
+                    <th>Units in stock</th>
+                    <th>Units on order</th>
+                    <th>Supplier</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(item, index) in products" :key="index">
+                    <td>{{ item.id }}</td>
+                    <td>{{ item.product_name }}</td>
+                    <td>{{ item.category.name }}</td>
+                    <td>{{ item.unit_price }}</td>
+                    <td>{{ item.units_in_stock }}</td>
+                    <td>{{ item.units_on_order }}</td>
+                    <td>{{ item.supplier.company_name }}</td>
+                    <td>
+                        <span>
+                            <Edit_Icon class="table_icon"/>
+                        </span>
+                        <span>
+                            <Trash_Icon class="table_icon__left"/>
+                        </span>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { loadProducts } from '@/api/reporting';
+import { defineComponent, onMounted, ref } from 'vue';
+import Edit_Icon from '@/assets/icons/Edit_Icon.vue';
+import Trash_Icon from '@/assets/icons/Trash_Icon.vue';
 
 export default defineComponent ({
-    
+    components: {
+        Edit_Icon,
+        Trash_Icon
+    },
+
+    setup() {
+        const products = ref()
+        const getProducts = async () => {
+            products.value = await loadProducts()
+        }
+
+        onMounted(() => {
+            getProducts()
+        })
+
+        return {
+            products
+        }
+    }
 })
 </script>
 
