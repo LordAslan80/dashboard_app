df8be798cce5b8504e9da593cfdd84c0eac9e953
Lord.Aslan <aslani80.m@gmail.com>
2026-04-09 13:22:04 +0330

    add categories get api url path

---
+++ frontend/src/api/common/categories.ts
@@ -0,0 +1,24 @@
+import { AxiosResponse, AxiosError } from "axios";
+import api from "../api";
+
+
+const URLS = {
+    categories: 'categories/'
+}
+
+export const loadCategories = () => {
+    return new Promise((resolve, reject) => {
+        api
+            .get(URLS.categories)
+            .then((response: AxiosResponse) => {
+                if(response.status === 200) {
+                    resolve(response.data)
+                } else {
+                    reject()
+                }
+            })
+            .catch((error: AxiosError) => {
+                console.log("caught error in load categories ===> ", error)
+            })
+    })
+}
