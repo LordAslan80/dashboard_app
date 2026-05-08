593acbfc9b9c5c2b4dde2b2af5722c951a67dcfa
Lord.Aslan <aslani80.m@gmail.com>
2026-05-08 13:20:27 +0330

    add config for get requests

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -11,7 +11,7 @@ const api = (axios: Axios) => {
     axios.defaults.baseURL = baseUrl + "/api"
 
     return {
-        get: <T>(url: string) => axios.get<T>(url),
+        get: <T>(url: string, config: any) => axios.get<T>(url, {...config}),
         post: <T>(url: string, body: object) => axios.post<T>(url, body),
         put: <T>(url: string, body: object) => axios.put<T>(url, body),
         patch: <T>(url: string, body: object) => axios.patch<T>(url, body),
