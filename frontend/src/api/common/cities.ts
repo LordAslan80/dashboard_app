e412a5963a0d37bcf8a82139228a980326423f05
Lord.Aslan <aslani80.m@gmail.com>
2026-05-08 12:13:49 +0330

    add load-cities api call (get)

---
+++ frontend/src/api/common/cities.ts
@@ -0,0 +1,23 @@
+import api from "../api";
+import { AxiosResponse, AxiosError } from "axios";
+
+
+const URLS = {
+    cities: "city-filters/"
+}
+
+export const loadCities = () => {
+    return new Promise((resolve, reject) => {
+        api.get(URLS.cities)
+        .then((response: AxiosResponse) => {
+            if(response.status === 200) {
+                resolve(response.data)
+            } else {
+                reject()
+            }
+        })
+        .catch((error: AxiosError) => {
+            console.log('caught error in load cities ===> ', error);
+        })
+    })
+}
