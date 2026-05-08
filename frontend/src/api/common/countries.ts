dc21b10c84e73782288b17297b85817f5a789c28
Lord.Aslan <aslani80.m@gmail.com>
2026-05-08 12:00:51 +0330

    add load-countries api call (get)

---
+++ frontend/src/api/common/countries.ts
@@ -0,0 +1,23 @@
+import api from "../api";
+import { AxiosResponse, AxiosError } from "axios";
+
+
+const URLS = {
+    countries: "country-filters/"
+}
+
+export const loadCountries = () => {
+    return new Promise((resolve, reject) => {
+        api.get(URLS.countries)
+        .then((response: AxiosResponse) => {
+            if(response.status === 200) {
+                resolve(response.data)
+            } else {
+                reject()
+            }
+        })
+        .catch((error: AxiosError) => {
+            console.log('caught error in load countries ===> ', error);
+        })
+    })
+}
