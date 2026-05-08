db90730404123afc3f5bd5e0c537ba8f1942eac0
Lord.Aslan <aslani80.m@gmail.com>
2026-05-08 12:12:47 +0330

    add city-filters url to backend

--- backend/reporting/urls.py
+++ backend/reporting/urls.py
@@ -19,6 +19,7 @@ router.register("products", ProductViewSet, basename="products")
 router.register("customers", CustomerViewSet, basename="customers")
 router.register("suppliers", SupplierViewSet, basename="suppliers")
 router.register("country-filters", CountryFilterViewSet, basename="country-filters")
+router.register("city-filters", CityFilterViewSet, basename="city-filters")
 router.register("product-filters", ProductFilterViewSet, basename="product-filters")
 
 
