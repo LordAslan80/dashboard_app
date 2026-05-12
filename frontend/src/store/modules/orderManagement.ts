a344157dc3d6a91a037b59e7ede7fa656eb1c32a
Lord.Aslan <aslani80.m@gmail.com>
2026-05-12 15:27:22 +0330

    use missing payload

--- frontend/src/store/modules/orderManagement.ts
+++ frontend/src/store/modules/orderManagement.ts
@@ -64,7 +64,7 @@ export default {
             commit("POST_ORDER", payload)
         },
         async setNumberOfPages({commit}: {commit: Commit}, payload: any) {
-            commit("SET_NUMBER_OF_PAGES")
+            commit("SET_NUMBER_OF_PAGES", payload)
         }
     },
     getters: {
