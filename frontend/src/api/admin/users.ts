9f7ba8bc2bed5a9968697c1c3a474973db4c1db9
Lord.Aslan <aslani80.m@gmail.com>
2026-05-27 12:08:34 +0330

    add user login endpoint in frontend

---
+++ frontend/src/api/admin/users.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+import { ILoginCredentials } from "@/models/ILoginCredentials";
+
+export const authenticate = (creds: ILoginCredentials) => {
+    axios.defaults.headers.common["Content-Type"] = "application/json"
+    const baseUrl = process.env.VUE_APP_BASE_URL
+
+    return axios
+        .post(`${baseUrl}/api/token/`, {
+            username: creds.username,
+            password: creds.password
+        }, {})
+        .then((response) => {
+            return response.status === 200
+            ? Promise.resolve(response)
+            : Promise.reject()
+        })
+        .catch((error) => {
+            if (error.message === "Network Error") return error.message
+            return error.response
+        })
+}
