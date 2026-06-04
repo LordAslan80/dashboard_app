dbbb5eca8cfbb835301a8db5100836f69ab34fc6
Lord.Aslan <aslani80.m@gmail.com>
2026-06-04 17:57:37 +0330

    add admin page sample

---
+++ frontend/src/views/administration/AdminView.vue
@@ -0,0 +1,23 @@
+<template>
+    <h1>Hello Admin!</h1>
+</template>
+
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent ({
+    components: {
+    },
+
+    setup() {
+
+        return {
+        }
+    },
+})
+</script>
+
+
+<style lang="scss">
+</style>
