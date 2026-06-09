5acaf1843fbd965ea72ea6af9d34843a3cb583cf
Lord.Aslan <aslani80.m@gmail.com>
2026-06-09 18:46:54 +0330

    use users overview page inside admin page

--- frontend/src/views/administration/AdminView.vue
+++ frontend/src/views/administration/AdminView.vue
@@ -1,13 +1,17 @@
 <template>
-    <h1>Hello Admin!</h1>
+    <div>
+        <users-overview class="users"/>
+    </div>
 </template>
 
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import UsersOverview from './screens/UsersOverview.vue';
 
 export default defineComponent ({
     components: {
+        UsersOverview
     },
 
     setup() {
@@ -20,4 +24,12 @@ export default defineComponent ({
 
 
 <style lang="scss">
+.users {
+    position: absolute;
+    height: 30%;
+    bottom: 20px;
+    width: 78%;
+    margin: .5rem .5rem 1rem;
+    overflow-x: scroll;
+}
 </style>
