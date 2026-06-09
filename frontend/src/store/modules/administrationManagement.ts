31315ce860b82578f8f99ced96a113d23ce4bf31
Lord.Aslan <aslani80.m@gmail.com>
2026-06-09 18:45:38 +0330

    create administration management for vuex state manegement

---
+++ frontend/src/store/modules/administrationManagement.ts
@@ -0,0 +1,32 @@
+import { Commit } from "vuex";
+import { getUsers } from "@/api/admin/admin";
+import { IUser } from "@/models/IUser";
+import { GlobalState } from "../types";
+
+export default {
+    namespaced: true,
+    state: {
+        users: []
+    },
+    mutations: {
+        SET_USERS(state: GlobalState, context: IUser[]) {
+            state.users = context
+        }
+    },
+    actions: {
+        setUsers({commit}: {commit: Commit}) {
+            getUsers()
+                .then((data) => {
+                    commit("SET_USERS", data)
+                })
+                .catch(() => {
+                    return false
+                })
+        }
+    },
+    getters: {
+        getUsers(state: GlobalState){
+            return state.users
+        }
+    }
+}
