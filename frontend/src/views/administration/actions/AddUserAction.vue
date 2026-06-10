01a43b4d3bb790f455f48959b55ac4379f7b27be
Lord.Aslan <aslani80.m@gmail.com>
2026-06-10 19:13:55 +0330

    create add new user action layout

--- frontend/src/views/administration/actions/AddUserAction.vue
+++ frontend/src/views/administration/actions/AddUserAction.vue
@@ -1,5 +1,42 @@
 <template>
-    <h1>Add user</h1>
+    <div class="admin-action">
+        <div class="admin-action__title">
+            <h3><b>New user</b></h3>
+        </div>
+
+        <div class="admin-action__content">
+            <form>
+                <div>
+                    <label for="username">Username</label>
+                    <input type="text" name="username" required>
+                </div>
+                <div>
+                    <label for="firstName">First name</label>
+                    <input type="text" name="firstName" required>
+                </div>
+                <div>
+                    <label for="lastName">Last name</label>
+                    <input type="text" name="lastName" required>
+                </div>
+                <div>
+                    <label for="email">Email</label>
+                    <input type="email" name="email" required>
+                </div>
+                <div>
+                    <label for="password">Password</label>
+                    <input type="password" name="password" required>
+                </div>
+                <div>
+                    <label for="isAdmin">Is Admin</label>
+                    <select name="isAdmin" id="" required>
+                        <option :value="1">Yse</option>
+                        <option :value="0" selected>No</option>
+                    </select>
+                </div>
+                <button class="button is-primary">Save</button>
+            </form>
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
@@ -15,4 +52,6 @@ export default defineComponent ({
 })
 </script>
 
-<style lang="scss"></style>
+<style lang="scss" scoped>
+@import "@/styles/components/_administration.scss"
+</style>
