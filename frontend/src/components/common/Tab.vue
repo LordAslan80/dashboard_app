d739ebc77fade5f89ccae7102f4ce4d4294273d3
Lord.Aslan <aslani80.m@gmail.com>
2026-06-10 18:14:35 +0330

    create structure of tabs

---
+++ frontend/src/components/common/Tab.vue
@@ -0,0 +1,28 @@
+<template>
+    <div v-if="title === selectedTab">
+        <slot></slot>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, inject } from 'vue';
+
+export default defineComponent ({
+    props: {
+        title: {
+            type: String,
+            default: "Tab"
+        }
+    },
+
+    setup() {
+        const selectedTab = inject("selectedTab")
+
+        return {
+            selectedTab
+        }
+    },
+})
+</script>
+
+<style lang="scss"></style>
