a9f70d84700664a861137a40168689a2200a3cf1
Lord.Aslan <aslani80.m@gmail.com>
2026-06-22 14:59:03 +0330

    create show & hide notifications system

---
+++ frontend/src/composables/outlets.ts
@@ -0,0 +1,33 @@
+import { markRaw, ref } from "vue";
+import { nanoid } from 'nanoid'
+import Notification from "@/components/outlets/Notification.vue";
+
+const outlets = ref([])
+
+export const getOutlets = () => outlets.value
+
+export const showOutlet = ({component, props, ...params}: any) => {
+    const outlet = {
+        outlet_id: nanoid(),
+        component: markRaw(component),
+        props,
+        ...params
+    }
+
+    outlets.value = outlets.value.concat(outlet)
+}
+
+export const hideOutlet = (outlet_id: string) => {
+    outlets.value = outlets.value.filter(
+        (outlet: any) => outlet.outlet_id !== outlet_id
+    )
+}
+
+export const showNotification = ({props, ...params}: any) => {
+    showOutlet({
+        component: Notification,
+        props,
+        type: "notification",
+        ...params
+    })
+}
