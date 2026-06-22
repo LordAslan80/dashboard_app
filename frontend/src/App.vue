eaa1fe3243ce8d52585c2146f3be1200408172f3
Lord.Aslan <aslani80.m@gmail.com>
2026-06-22 15:06:03 +0330

    use outlets to show in app.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <router-view v-if="logged"/>
    <login-view v-else/>
+   <outlets/>
 </template>
 
 <script lang="ts">
 import { computed, defineComponent } from 'vue';
 import LoginView from './views/user/LoginView.vue';
 import { get as getFromStore, load as loadFromStore } from './localStorage';
+import Outlets from './components/outlets/Outlets.vue';
 
 export default defineComponent({
   components: {
-    LoginView
+    LoginView,
+    Outlets
   },
   setup() {
     const logged = computed(() => getFromStore('logged'))
